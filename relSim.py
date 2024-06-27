fc83b584fc6cab2953e61aef40c6c52e1d53cab6
Paul <paulrbhughes@outlook.com>
2024-06-27 18:55:29 -0400

    added simulation with delta and average n - theres a discrepancy RN

--- relSim.py
+++ relSim.py
@@ -10,21 +10,21 @@ plt.rcParams.update({
 
 zeta = 0.6
 g0 = 0.75
-n1b = 0.15
-n2b = 0.25
-ic = np.array([n1b, n2b, 0])
-target = 1e-12
+av_bath = 0.2
+diff_bath = 0.1
+ic = np.array([av_bath, diff_bath, 0])
+target = 1e-8
 tf = 10
 
 fig, ax = plt.subplots(2)
-t, state = utils.simulation(zeta, g0, n1b, n2b, ic, target, tf)
+t, state = utils.rel_simulation(zeta, g0, av_bath, diff_bath, ic, target, tf)
 print(np.size(t))
-corr = utils.correlation_var(state)
+corr = utils.rel_corr_var(state)
 ax[0].plot(t, corr, linewidth=2)
 ax[0].set_xlabel(r"$\tilde{t}$")
 ax[0].set_ylabel("$\Delta_{12}^2$")
 ax[0].hlines(1, 0, tf, linewidth=1.5, linestyles=":")
-ax[1].plot(t, 0.5 * (state[1] + state[0]), linewidth=2)
+ax[1].plot(t, state[0], linewidth=2)
 ax[1].set_xlabel(r"$\tilde{t}$")
 ax[1].set_ylabel(r"$\bar{n}$")
 plt.tight_layout()
--- utils.py
+++ utils.py
@@ -26,8 +26,20 @@ def eom(zeta, g, batha, bathb, state):
     u = state[2]
     dn1 = thermal_eom(zeta, batha, bathb, na, u)
     dn2 = thermal_eom(-zeta, bathb, batha, nb, u)
-    du = 0.5*g - np.cosh(u) * np.sinh(u) * (batha + bathb + 1 + zeta*(batha - na + nb - bathb))/(na + nb + 1)
+    du = 0.5*(g - np.sinh(2*u) * (batha + bathb + 1 + zeta*(batha - na + nb - bathb))/(na + nb + 1))
     return np.array([dn1, dn2, du])
+
+
+def rel_eom(zeta, g, av_bath, diff_bath, state):
+    # same as above but uses the average and difference numbers
+    #state = [naverage, ndifference, u]
+    av = state[0]
+    diff = state[1]
+    u = state[2]
+    dav = 0.5 * ((2*av_bath + 1)*np.cosh(2*u) + zeta*(diff - diff_bath) - 2*av - 1)
+    ddiff = diff_bath - diff - zeta*(2*av_bath + 1 - np.cosh(2*u) * (2*av + 1))
+    du = 0.5 * (g - np.sinh(2*u)*(2*av_bath + 1 + zeta*(diff - diff_bath))/(2*av + 1))
+    return np.array([dav, ddiff, du])
 
 
 def rk4slope(zeta, g, batha, bathb, state, dt):
@@ -72,9 +84,53 @@ def simulation(zeta, g, bath1, bath2, initial, target, tf):
     return t, state
 
 
+def rel_rk4slope(zeta, g, avb, db, state, dt):
+    k1 = rel_eom(zeta, g, avb, db, state)
+    k2 = rel_eom(zeta, g, avb, db, state + 0.5 * k1 * dt)
+    k3 = rel_eom(zeta, g, avb, db, state + 0.5 * k2 * dt)
+    k4 = rel_eom(zeta, g, avb, db, state + k3 * dt)
+    return k1/6 + k2/3 + k3/3 + k4/6
+
+
+def rel_simulation(zeta, g, avb, db, initial, target, tf):
+    # same as above w
+    dt = 0.01  # arb
+    t = np.zeros(100000000)  # arb
+    state = np.zeros([3, 100000000])
+    state[:, 0] = initial[:]
+    i = 0
+    while t[i] < tf:  # standard dynamic timestep trick
+        test = 30 * dt * target
+        # two steps of dt
+        k = rel_rk4slope(zeta, g, avb, db, state[:, i], dt)
+        check1a = state[:, i] + k * dt
+        k = rel_rk4slope(zeta, g, avb, db, check1a, dt)
+        check1 = check1a + k * dt
+        # one step of 2dt
+        k = rel_rk4slope(zeta, g, avb, db, state[:, i], 2*dt)
+        check2 = state[:, i] + 2 * k * dt
+        # checking error estimate
+        check = np.max(np.absolute(check1[:2] - check2[:2]))  # only care about populations
+        if check < 0.5 * test:
+            rho = 2
+        else:
+            rho = test/check  # taking faith in old code
+        if rho > 1:
+            t[i + 1] = t[i] + dt
+            state[:, i + 1] = check1a
+            i += 1
+        dt = dt * rho**0.25
+
+    t = t[:i-1]
+    state = state[:, :i - 1]
+    return t, state
+
+
 def correlation_var(state):
-    return (1 + state[0,:] + state[1,:]) * np.exp(-2 * state[2,:])
+    return (1 + state[0, :] + state[1, :]) * np.exp(-2 * state[2, :])
 
 
-
+def rel_corr_var(state):
+    # same as above but using other n
+    return (2*state[0, :] + 1)*np.exp(-2*state[2, :])
 
