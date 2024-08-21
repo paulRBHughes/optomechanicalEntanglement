b0a0ea2f892cfdce5f831727d05dd97782af10ef
Paul <paulrbhughes@outlook.com>
2024-08-21 16:40:08 -0400

    includes the missed sin term in theta

--- coolingutils.py
+++ coolingutils.py
@@ -14,59 +14,47 @@ Need to use the full equation set (for now, since gamma is static - interaction)
 """
 
 
-@njit
-def rotating(gamma, phi):
-    g = gamma * np.exp(-1j * phi)
-    return [(g + np.conj(g))/2, (g - np.conj(g))/2]
-
-
 # @njit
-def eom(deltOmega, zeta, gamma, nb1, nb2, state):
+def eom(zeta, gamma, nb1, nb2, state):
     n1 = state[0]
     n2 = state[1]
     theta = state[2]
-    phi = state[3]
-    pump = rotating(gamma, phi)
     deltan = n2 - n1
     deltanb = nb2 - nb1
     twonth = n1 + n2
     twonb = nb1 + nb2
-    dn1 = (1 + zeta)*nb1*np.square(np.cos(theta)) + (1 - zeta)*nb2*np.square(np.sin(theta)) - (zeta* np.cos(2*theta) + 1) * n1
-    dn2 = (1 - zeta) * nb2 * np.square(np.cos(theta)) + (1 + zeta) * nb1 * np.square(np.sin(theta)) - (1 -
-                zeta * np.cos(2 * theta)) * n2
-    dtheta = -pump[0] + (2*zeta*(twonb - twonth) - deltanb)/deltan
-    # if theta < 1e-5:
-    #     dphi = deltOmega
-    # else:
-    #     dphi = deltOmega + 1j * pump[1]/np.tan(2*theta)
-    dphi = 0
-    return np.array([dn1, dn2, dtheta, dphi])
+    dn1 = ((1 + zeta) * nb1 * np.square(np.cos(theta)) + (1 - zeta) * nb2 * np.square(np.sin(theta)) -
+           (1 + zeta * np.cos(2 * theta)) * n1)
+    dn2 = ((1 - zeta) * nb2 * np.square(np.cos(theta)) + (1 + zeta) * nb1 * np.square(np.sin(theta)) -
+           (1 - zeta * np.cos(2 * theta)) * n2)
+    dtheta = -np.abs(gamma) + (zeta * (twonb - twonth) - deltanb) * np.sin(2 * theta)/deltan
+    return np.array([dn1, dn2, dtheta])
 
 
-def rk4slope(deltOmega, zeta, gamma, nb1, nb2, state, dt):
-    k1 = eom(deltOmega, zeta, gamma, nb1, nb2, state)
-    k2 = eom(deltOmega, zeta, gamma, nb1, nb2, state + 0.5 * k1 * dt)
-    k3 = eom(deltOmega, zeta, gamma, nb1, nb2, state + 0.5 * k2 * dt)
-    k4 = eom(deltOmega, zeta, gamma, nb1, nb2, state + k3 * dt)
+def rk4slope(zeta, gamma, nb1, nb2, state, dt):
+    k1 = eom(zeta, gamma, nb1, nb2, state)
+    k2 = eom(zeta, gamma, nb1, nb2, state + 0.5 * k1 * dt)
+    k3 = eom(zeta, gamma, nb1, nb2, state + 0.5 * k2 * dt)
+    k4 = eom(zeta, gamma, nb1, nb2, state + k3 * dt)
     return k1/6 + k2/3 + k3/3 + k4/6
 
 
-def simulation(deltOmega, zeta, gamma, nb1, nb2, initial, target, tf):
+def simulation(zeta, gamma, nb1, nb2, initial, target, tf):
     # target will be the target precision of a thermal population
     dt = 0.01  # arb
     t = np.zeros(100000000)  # arb
-    state = np.zeros([4, 100000000])
+    state = np.zeros([3, 100000000])
     state[:, 0] = initial[:]
     i = 0
     while t[i] < tf:  # standard dynamic timestep trick
         test = 30 * dt * target
         # two steps of dt
-        k = rk4slope(deltOmega, zeta, gamma, nb1, nb2, state[:, i], dt)
+        k = rk4slope(zeta, gamma, nb1, nb2, state[:, i], dt)
         check1a = state[:, i] + k * dt
-        k = rk4slope(deltOmega, zeta, gamma, nb1, nb2, check1a, dt)
+        k = rk4slope(zeta, gamma, nb1, nb2, check1a, dt)
         check1 = check1a + k * dt
         # one step of 2dt
-        k = rk4slope(deltOmega, zeta, gamma, nb1, nb2, state[:, i], 2*dt)
+        k = rk4slope(zeta, gamma, nb1, nb2, state[:, i], 2*dt)
         check2 = state[:, i] + 2 * k * dt
         # checking error estimate
         check = np.max(np.absolute(check1[:2] - check2[:2]))  # only care about populations
--- simulation.py
+++ simulation.py
@@ -8,34 +8,40 @@ plt.rcParams.update({
         "font.size": "14"
     })
 
-# T = 300
-# omega1 = 300e12
-# omega2 = 300e9
-deltOmega = 0
-zeta = 0.98
-gamma = 0.1
-nb1 = 0
-nb2 = 2
-initial = np.array([nb1, nb2, 0, np.pi/2])
+
+zeta = 0.8
+gamma = 2
+nb1 = 0.
+nb2 = 4
+initial = np.array([nb1, nb2, 0])
 target = 1e-8
 tf = 5
+# thetass = 0.5 * np.arctan(-2 * gamma)
+steady = (nb1 + nb2) * 0.5 - zeta * (nb2 - nb1) * 0.5
 
-t, state = coolingutils.simulation(deltOmega, zeta, gamma, nb1, nb2, initial, target, tf)
 
-fig, ax = plt.subplots(2)
-ax[0].plot(t, state[0]*np.square(np.cos(state[2])) + state[1]*np.square(np.sin(state[2])), linewidth=2)
+t, state = coolingutils.simulation(zeta, gamma, nb1, nb2, initial, target, tf)
+
+fig, ax = plt.subplots(3)
+ax[0].plot(t, state[0]*np.square(np.cos(state[2])) + state[1]*np.square(np.sin(state[2])), linewidth=2, label=r"$n_c$")
+ax[0].plot(t, state[1]*np.square(np.cos(state[2])) + state[0]*np.square(np.sin(state[2])), linewidth=2, label=r"$n_m$")
+ax[0].hlines(steady, 0, tf, linewidth=1.5, linestyles=":", label="$n_{ss}$")
 ax[0].set_xlabel(r"$\tilde{t}$")
-ax[0].set_ylabel("$n_{1}$")
-# ax[0].hlines(1, 0, tf, linewidth=1.5, linestyles=":")
-ax[1].plot(t, state[1]*np.square(np.cos(state[2])) + state[0]*np.square(np.sin(state[2])), linewidth=2, label=zeta)
-ax[1].set_xlabel(r"$\tilde{t}$")
-ax[1].set_ylabel(r"$n_2$")
-# ax[1, 0].plot(t, state[2]%np.pi, linewidth=2, label=zeta)
-# ax[1, 0].set_xlabel(r"$\tilde{t}$")
-# ax[1, 0].set_ylabel(r"$\theta$")
-# ax[1, 1].plot(t, state[3]%np.pi, linewidth=2, label=zeta)
-# ax[1, 1].set_xlabel(r"$\tilde{t}$")
-# ax[1, 1].set_ylabel(r"$\phi$")
-plt.tight_layout()
-plt.show()
+ax[0].set_ylabel("$n$")
+ax[0].legend()
 
+
+ax[1].plot(t, state[0], linewidth=2, label=r"$n_c^{th}$")
+ax[1].plot(t, state[1], linewidth=2, label=r"$n_m^{th}$")
+ax[1].hlines(steady, 0, tf, linewidth=1.5, linestyles=":", label="$n^{th}_{ss}$")
+ax[1].set_xlabel(r"$\tilde{t}$")
+ax[1].set_ylabel(r"$n_{th}$")
+ax[1].legend()
+
+ax[2].plot(t, state[2] % (2 * np.pi), linewidth=2, label=zeta)
+ax[2].set_xlabel(r"$\tilde{t}$")
+ax[2].set_ylabel(r"$\theta$")
+
+plt.tight_layout()
+plt.savefig("mechanicalCooling.pdf", format='pdf', dpi=1200, bbox_inches='tight')
+# plt.show()
